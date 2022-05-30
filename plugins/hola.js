0bebe619f321b9faf52d32e696a825084e7bd452
ColapsusHD <106274886+ColapsusHD@users.noreply.github.com>
2022-05-29 23:32:21 -0300

    Update hola.js

--- plugins/hola.js
+++ plugins/hola.js
@@ -2,8 +2,8 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/bot.mp3'
-conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, {
+let vn = './media/Hola amigos como están que introducción épica no Dross.mp3'
+conn.sendFile(m.chat, vn, 'Hola amigos como están que introducción épica no Dross.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })

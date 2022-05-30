e1cf4806a5e1653e8fe66ad74178e07cde8b5d5a
ColapsusHD <106274886+ColapsusHD@users.noreply.github.com>
2022-05-30 00:42:02 -0300

    Update menu2-audios.js

--- plugins/menu2-audios.js
+++ plugins/menu2-audios.js
@@ -22,129 +22,150 @@ let handler = async (m, { conn, usedPrefix }) => {
 ⎪≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 ⎪ ✢ Solo escriba la palabra/frase. sin prefijo ⎪ (/ . # etc...) 
 ⎪≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-⎪➫🔊 _Noche de paz_
-⎪➫🔊 _Buenos dias_
-⎪➫🔊 _Audio hentai_
-⎪➫🔊 _Fiesta del admin_
-⎪➫🔊 _Fiesta del admin 2_
-⎪➫🔊 _Viernes_
-⎪➫🔊 _Me olvidé_
-⎪➫🔊 _Baneado_
-⎪➫🔊 _Feliz navidad_
-⎪➫🔊 _A nadie le importa_
-⎪➫🔊 _Sexo_
-⎪➫🔊 _Vete a la vrg_
-⎪➫🔊 _Ara ara_
-⎪➫🔊 _Hola_
-⎪➫🔊 _Un pato_
-⎪➫🔊 _Nyanpasu_
-⎪➫🔊 _Te amo_
-⎪➫🔊 _Yamete_
-⎪➫🔊 _Te diagnostico con gay_
-⎪➫🔊 _Quien es tu sempai botsito 7w7_
-⎪➫🔊 _Bañate_
-⎪➫🔊 _Vivan los novios_
-⎪➫🔊 _Marica quien_
-⎪➫🔊 _Es puto_
-⎪➫🔊 _La biblia_
-⎪➫🔊 _Onichan_
-⎪➫🔊 _Bot puto_
-⎪➫🔊 _Feliz cumpleaños_
-⎪➫🔊 _Pasa pack Bot_
+⎪➫🔊 _A_
 ⎪➫🔊 _Atencion grupo_
-⎪➫🔊 _Homero chino_
-⎪➫🔊 _Oh me vengo_
-⎪➫🔊 _Murio el grupo_
-⎪➫🔊 _Siuuu_
-⎪➫🔊 _Rawr_
-⎪➫🔊 _UwU_
-⎪➫🔊 _:c_
-⎪➫🔊 _a_
-⎪➫🔊 _Hey_
-⎪➫🔊 _Enojado_
-⎪➫🔊 _Enojada_
-⎪➫🔊 _Chao_
-⎪➫🔊 _Hentai_
-⎪➫🔊 _Triste_
-⎪➫🔊 _Estoy triste_
-⎪➫🔊 _Me pican los cocos_
-⎪➫🔊 _Contexto_
-⎪➫🔊 _Me voy_
-⎪➫🔊 _Tengo los calzones del admin_
-⎪➫🔊 _Entrada épica_ 
-⎪➫🔊 _Esto va ser épico papus_
-⎪➫🔊 _Ingresa épicamente_
+⎪➫🔊 _Audio hentai_
+⎪➫🔊 _A nadie le importa_
+⎪➫🔊 _Ara ara_
+⎪➫🔊 _Among us_
+⎪➫🔊 _Abduzcan_
+⎪➫🔊 _Buenos dias_
+⎪➫🔊 _Baneado_
+⎪➫🔊 _Bañate_
+⎪➫🔊 _Bot puto_
 ⎪➫🔊 _Bv_
-⎪➫🔊 _Yoshi_
-⎪➫🔊 _No digas eso papu_
-⎪➫🔊 _Ma ma masivo_
-⎪➫🔊 _Masivo_
 ⎪➫🔊 _Basado_
 ⎪➫🔊 _Basada_
-⎪➫🔊 _Fino señores_
-⎪➫🔊 _Verdad que te engañe_
-⎪➫🔊 _Sus_
-⎪➫🔊 _Ohayo_
-⎪➫🔊 _La voz de hombre_
-⎪➫🔊 _Pero esto_
 ⎪➫🔊 _Bien pensado Woody_
-⎪➫🔊 _Jesucristo_
-⎪➫🔊 _Wtf_
-⎪➫🔊 _Una pregunta_
-⎪➫🔊 _Que sucede_
-⎪➫🔊 _Hablame_
-⎪➫🔊 _Pikachu_
-⎪➫🔊 _Niconico_
-⎪➫🔊 _Yokese_
-⎪➫🔊 _Omaiga_
-⎪➫🔊 _Nadie te preguntó_
 ⎪➫🔊 _Bueno si_
-⎪➫🔊 _Usted está detenido_
-⎪➫🔊 _No me hables_
-⎪➫🔊 _No chu_
-⎪➫🔊 _El pepe_
-⎪➫🔊 _Pokémon_
-⎪➫🔊 _No me hagas usar esto_
-⎪➫🔊 _Esto va para ti_
-⎪➫🔊 _Abduzcan_
-⎪➫🔊 _Joder_
-⎪➫🔊 _Hablar primos_
-⎪➫🔊 _Mmm_
-⎪➫🔊 _Orale_
-⎪➫🔊 _Me anda buscando anonymous_
-⎪➫🔊 _Blackpink in your area_
-⎪➫🔊 _Cambiate a Movistar_
-⎪➫🔊 _Momento equisde | Momento XD_
-⎪➫🔊 _Todo bien | 🧐_
-⎪➫🔊 _Te gusta el Pepino | 🥒_
-⎪➫🔊 _El tóxico_
-⎪➫🔊 _Moshi moshi_
-⎪➫🔊 _Calla Fan de BTS_
-⎪➫🔊 _Que tal grupo_
-⎪➫🔊 _Muchachos_
-⎪➫🔊 _Está Zzzz_
-⎪➫🔊 _Goku Pervertido_
-⎪➫🔊 _Potaxio | 🥑_
-⎪➫🔊 _Nico nico_
-⎪➫🔊 _El rap de Fernanfloo_
-⎪➫🔊 _Tal vez_
-⎪➫🔊 _Corte corte_
 ⎪➫🔊 _Buenas noches_
-⎪➫🔊 _Porque ta tite_
-⎪➫🔊 _Eres Fuerte_
 ⎪➫🔊 _Bueno Master | 🫂_
-⎪➫🔊 _No Rompas más | 💔_
-⎪➫🔊 _Traiganle una falda_
-⎪➫🔊 _Se están riendo de mí_
-⎪➫🔊 _Su nivel de pendejo_
-⎪➫🔊 _Bienvenido/a | 🥳 | 🤗 | 👋_
-⎪➫🔊 _Elmo sabe donde vives_
-⎪➫🔊 _tunometecabrasaramambiche_
-⎪➫🔊 _Y este quien es_
-⎪➫🔊 _Motivación_
-⎪➫🔊 _En caso de una investigación_
+⎪➫🔊 _Bienvenido/a | 🤗_
 ⎪➫🔊 _Buen día grupo | 🙌_
+⎪➫🔊 _Blackpink in your area_
+⎪➫🔊 _Corte corte_
+⎪➫🔊 _Calla Fan de BTS_
+⎪➫🔊 _Cambiate a Movistar_
+⎪➫🔊 _Contexto_
+⎪➫🔊 _Callate | 🤫_
+⎪➫🔊 _Como??? | 🚨_
+⎪➫🔊 _Chau | 👋_
+⎪➫🔊 _DIVINO | 💋_
+⎪➫🔊 _Depresion_
+⎪➫🔊 _Diablos Señorita | 😈_
+⎪➫🔊 _En caso de una investigación_
+⎪➫🔊 _Elmo sabe donde vives_
+⎪➫🔊 _Eres Fuerte_
+⎪➫🔊 _El rap de Fernanfloo_
+⎪➫🔊 _Está Zzzz_
+⎪➫🔊 _El tóxico_
+⎪➫🔊 _Esto va para ti_
+⎪➫🔊 _El pepe_
+⎪➫🔊 _Esto va ser épico papus_
+⎪➫🔊 _Entrada épica_ 
+⎪➫🔊 _Estoy triste_
+⎪➫🔊 _Enojada_
+⎪➫🔊 _Enojado_
+⎪➫🔊 _Es puto_
+⎪➫🔊 _Fino señores_
+⎪➫🔊 _Feliz cumpleaños | 🥳 | 🎉_
+⎪➫🔊 _Feliz navidad_
+⎪➫🔊 _Fiesta del admin_
+⎪➫🔊 _Fiesta del admin 2_
+⎪➫🔊 _Hablar primos_
+⎪➫🔊 _Hablame_
+⎪➫🔊 _Hentai_
+⎪➫🔊 _Hey_
+⎪➫🔊 _Homero chino_
+⎪➫🔊 _Hola_
+⎪➫🔊 _Ingresa épicamente_
+⎪➫🔊 _Jijija | 😂_
+⎪➫🔊 _Joder_
+⎪➫🔊 _Jesucristo_
+⎪➫🔊 _Leche | 🥛_
+⎪➫🔊 _La roca | 🤨_
 ⎪➫🔊 _Las reglas del grupo_
+⎪➫🔊 _La voz de hombre_
+⎪➫🔊 _La biblia_
+⎪➫🔊 _LESS GO_
+⎪➫🔊 _Motivación_
+⎪➫🔊 _Muchachos_
+⎪➫🔊 _Moshi moshi_
+⎪➫🔊 _Momento equisde | Momento XD_
+⎪➫🔊 _Me anda buscando anonymous_
+⎪➫🔊 _Mmm_
+⎪➫🔊 _Ma ma masivo_
+⎪➫🔊 _Masivo_
+⎪➫🔊 _Me voy_
+⎪➫🔊 _Me pican los cocos_
+⎪➫🔊 _Murio el grupo_
+⎪➫🔊 _Marica quien_
+⎪➫🔊 _Me olvidé_
+⎪➫🔊 _Nya | 🐱_
+⎪➫🔊 _Noche de paz_
+⎪➫🔊 _Noche de paz_
+⎪➫🔊 _No Rompas más | 💔_
+⎪➫🔊 _Nico nico_
+⎪➫🔊 _No me hagas usar esto_
+⎪➫🔊 _No chu_
+⎪➫🔊 _No me hables_
+⎪➫🔊 _Nadie te preguntó_
+⎪➫🔊 _Niconico_
+⎪➫🔊 _No digas eso papu_
+⎪➫🔊 _Nyanpasu_
+⎪➫🔊 _Odio a Chile_
+⎪➫🔊 _Orale_
+⎪➫🔊 _Omaiga_
+⎪➫🔊 _Ohayo_
+⎪➫🔊 _Oh me vengo_
+⎪➫🔊 _Onichan_
+⎪➫🔊 _Porque me excita tanto | 🥵_
+⎪➫🔊 _Porque ta tite_
+⎪➫🔊 _Potaxio | 🥑_
+⎪➫🔊 _Pokémon_
+⎪➫🔊 _Pikachu_
+⎪➫🔊 _Pero esto_
+⎪➫🔊 _Pasa pack Bot_
+⎪➫🔊 _Puta que rico | 🍑_
+⎪➫🔊 _Que dice? | 😐_
+⎪➫🔊 _Que tal grupo_
+⎪➫🔊 _Que sucede_
+⎪➫🔊 _Quien es tu sempai botsito 7w7_
+⎪➫🔊 _Rawr_
+⎪➫🔊 _Salimo en caravana_
+⎪➫🔊 _Sexo_
+⎪➫🔊 _Su nivel de pendejo_
+⎪➫🔊 _Se están riendo de mí_
+⎪➫🔊 _Siuuu_
+⎪➫🔊 _Troleado_
+⎪➫🔊 _Toy Chica_
+⎪➫🔊 _Traiganle una falda_
+⎪➫🔊 _Tal vez_
+⎪➫🔊 _Te gusta el Pepino | 🥒_
+⎪➫🔊 _Todo bien | 🧐_
+⎪➫🔊 _Tengo los calzones del admin_
+⎪➫🔊 _Triste_
+⎪➫🔊 _Te diagnostico con gay_
+⎪➫🔊 _Te amo_
+⎪➫🔊 _Tunometecabrasaramambiche_
+⎪➫🔊 _UwU_
+⎪➫🔊 _Un pato_
+⎪➫🔊 _Una pregunta_
+⎪➫🔊 _Usted está detenido_
+⎪➫🔊 _Viernes_
+⎪➫🔊 _Vete a la vrg_
+⎪➫🔊 _Verdad que te engañe_
+⎪➫🔊 _Vivan los novios_
+⎪➫🔊 _Wenomechainsama | 🐹_
+⎪➫🔊 _Wtf_
+⎪➫🔊 _Yumbi yumbi | 🤠_
+⎪➫🔊 _Y Digo Wo_
+⎪➫🔊 _Yamete_
+⎪➫🔊 _Yoshi_
+⎪➫🔊 _Yokese_
+⎪➫🔊 _Ya antojaron_
+⎪➫🔊 _Y este quien es_
+⎪➫🔊 _:c_
 ╰══〘 🐈⚡️🐈⚡️🐈⚡️🐈 〙══╯
 
 ╭════• ೋ•✧๑♡๑✧•ೋ •════╮
